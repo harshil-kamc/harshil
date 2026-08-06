fdd0542f74afdf503bf16d13d7c9fc7a685316a1
Harshil Kamchetty <245124751024@mvsrec.edu.in>
2026-08-06 13:41:33 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/h3d/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
