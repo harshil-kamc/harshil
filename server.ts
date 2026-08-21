9719346ae3f4ae4e97991a1027818a9ca1591fc2
Harshil Kamchetty <245124751024@mvsrec.edu.in>
2026-08-21 15:12:57 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // JSON Body parser
   app.use(express.json());
